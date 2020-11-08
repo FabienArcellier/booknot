a5f0119bf0199f6d06d24c2b82e921d327d79142
Fabien Arcellier <fabien.arcellier@gmail.com>
2020-11-08 17:05:43 +0100

    * add toctree when create the booknot repository

--- booknot/application/init_application.py
+++ booknot/application/init_application.py
@@ -20,6 +20,11 @@ class InitApplication:
         try:
             if not os.path.isdir(booknot_directory):
                 shutil.copytree(os.path.join(RESOURCES_DIR, 'booknot_root'), os.path.join(self.directory, '.booknot'))
+
+            booknot_toctree = os.path.join(self.directory, 'index.rst')
+            if not os.path.isfile(booknot_toctree):
+                shutil.copy(os.path.join(RESOURCES_DIR, 'toctree.rst') ,booknot_toctree)
+
         except Exception as exception:
             if os.path.isdir(booknot_directory):
                 shutil.rmtree(booknot_directory)
